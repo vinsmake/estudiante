1879d67c6cefcf9074852db89d8018de9a54d409
319920ch <153756331+319920ch@users.noreply.github.com>
2025-02-05 16:37:02 -0500

    Update tiempo.js

--- models/tiempo.js
+++ models/tiempo.js
@@ -1,11 +1,11 @@
 // Función para actualizar el valor del rango en tiempo real
 function actualizarValor(valor) {
-    document.getElementById('valor-tiempo').textContent = valor + " horas";
+    document.getElementById('valor-tiempo').textContent = " Tiempo seleccionado: "+ valor + " h";
 }
 
 // Función para manejar el clic en el botón
 function generarSimulacion() {
-    const tiempo = document.getElementById('tiempo').value;
+    const tiempo = document.getElementById('customRange3').value;
     localStorage.setItem("horaSimulacion", tiempo);
     window.location.href = "interface.html"; // Redirige a la página interface.html
 }
