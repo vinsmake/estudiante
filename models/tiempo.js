be693aecbcde75599f6837f41c7f0eb4a14d92b5
TheTuxTron <joelinca16@gmail.com>
2025-02-07 22:05:36 -0500

    Funciona barra entre interfaces

--- models/tiempo.js
+++ models/tiempo.js
@@ -1,6 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const rango = document.getElementById("customRange3");
+    const valorGuardado = localStorage.getItem("horaSimulacion");
+
+    // Si hay un valor guardado, actualizar el input range
+    if (valorGuardado) {
+        rango.value = valorGuardado;
+        actualizarValor(valorGuardado); // También actualiza el texto mostrado
+    }
+});
+
 // Función para actualizar el valor del rango en tiempo real
-function actualizarValor(valor) {
-    document.getElementById('valor-tiempo').textContent = `Tiempo seleccionado: ${valor} h`;
+function actualizarValor() {
+    horaSimulacion = document.getElementById("customRange3").value;
+    document.getElementById('valor-tiempo').textContent = `Tiempo seleccionado: ${horaSimulacion} h`;
 }
 
 
@@ -9,4 +21,9 @@ function generarSimulacion() {
     const tiempo = document.getElementById('customRange3').value;
     localStorage.setItem("horaSimulacion", tiempo);
     window.location.href = "interface.html"; // Redirige a la página interface.html
+}
+
+function regresarIndex(){
+    localStorage.setItem("horaSimulacion", 1);
+    window.location.href = "index.html";
 }
